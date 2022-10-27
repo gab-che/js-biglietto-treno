a75210f998f8a83cff990e89a7195a8d29f53212
gab-che <gabriellachessa1@gmail.com>
2022-10-27 19:25:30 +0200

    added style to ticket

--- js/script.js
+++ js/script.js
@@ -4,17 +4,19 @@ const discountYoung = 0.8;
 const discountOld = 0.6;
 
 // riferimento html
-let priceTag = document.getElementById("pricetag");
+const priceTag = document.getElementById("pricetag");
+const disCount = document.getElementById("discount");
 
 // chiedo i km + li salvo in una variabile
-let userKm = parseInt(prompt("Quanti km devi fare?"));
+const userKm = parseInt(prompt("Quanti km devi fare?"));
+
 
 if (isNaN(userKm)){
     userKm = parseInt(prompt("Inserisci un numero valido"));
 }
 
 // chiedo l'età + metto in una variabile
-let userAge = parseInt(prompt("Quanti anni hai?"));
+const userAge = parseInt(prompt("Quanti anni hai?"));
 
 if (isNaN(userAge)){
     userAge = parseInt(prompt("Inserisci un numero valido"));
@@ -23,13 +25,24 @@ if (isNaN(userAge)){
 // diversi alert a seconda dell'età
 if (userAge < 18){
     // se minorenne sconto del 20% (* 0.8)
-    priceTag.innerHTML = (Math.floor(userKm * price * discountYoung).toFixed(2));
+    priceTag.innerHTML = (userKm * price * discountYoung).toFixed(2);
+    disCount.innerHTML = "20%"
 
 } else if( userAge >= 65){
     // se maggiore di 65 sconto del 40% (* 0.6)
-    priceTag.innerHTML = (Math.floor(userKm * price * discountOld).toFixed(2));
+    priceTag.innerHTML = (userKm * price * discountOld).toFixed(2);
+    disCount.innerHTML = "40%";
 
 } else{
     // altrimenti prezzo pieno
-    priceTag.innerHTML = (Math.floor(userKm * price).toFixed(2));
+    priceTag.innerHTML = (userKm * price).toFixed(2);
 }
+
+
+//------ altre info ---------
+const userName = document.getElementById("name");
+userName.innerHTML = prompt("Inserisci il tuo nome completo: ");
+
+const fullPrice = document.getElementById("fullprice");
+fullPrice.innerHTML = (userKm * price);
+
